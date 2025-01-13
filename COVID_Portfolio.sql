1e9dbcf86092a7844197fb51f6076059b6761638
rlfowler21 <rachellaurenfowler@gmail.com>
2025-01-13 17:16:09 -0500

    COVID_Portfolio.sql

--- COVID_Portfolio.sql
+++ COVID_Portfolio.sql
@@ -1,57 +1,92 @@
-Select *
-from `eighth-physics-440919-t4.Portfolio_Project.Covid_Deaths`
-order by 3,4
+/*
+Covid 19 Data Exploration 
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+*/
 
+--Dataset--
 
-select location, date, total_cases, new_cases, total_deaths, population 
-from `eighth-physics-440919-t4.Portfolio_Project.Covid_Deaths`
-order by 1,2
+SELECT *
+FROM 
+  `eighth-physics-440919-t4.Portfolio_Project.Covid_Deaths`
+ORDER BY 
+  3,4
 
--- Looking at Total Cases vs Total Deaths 
--- Shows probability of dying from contracting Covid in your country
-select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as Death_Percentage
-from `eighth-physics-440919-t4.Portfolio_Project.Covid_Deaths`
-where location = 'United States'
-order by 1,2
+-- Beginning Data --
+SELECT 
+  location, 
+  date, 
+  total_cases, 
+  new_cases, 
+  total_deaths, 
+  population 
+FROM 
+  `eighth-physics-440919-t4.Portfolio_Project.Covid_Deaths`
+ORDER BY 
+  1,2
 
--- Looking at Total Cases vs Population
--- Shows probability of contracting Covid
-select location, date,  population, total_cases, (total_cases/population)*100 as Population_Percentage_Infected
-from `eighth-physics-440919-t4.Portfolio_Project.Covid_Deaths`
-where location = 'United States'
-order by 1,2
+-- Total Cases vs Total Deaths --
+-- Shows probability of dying from contracting COVID in your country
+SELECT 
+  location, 
+  date, 
+  total_cases, 
+  total_deaths, 
+  (total_deaths/total_cases)*100 AS Death_Percentage
+FROM 
+  `eighth-physics-440919-t4.Portfolio_Project.Covid_Deaths`
+WHERE 
+  location = 'United States'
+ORDER BY 
+  1,2
 
--- Looking at Countries with Highest Infection Rate Per Population
+-- Total Cases vs Population --
+-- Shows probability of contracting COVID
+SELECT 
+  location, 
+  date,  
+  population,
+  total_cases,
+  (total_cases/population)*100 AS Population_Percentage_Infected
+FROM 
+  `eighth-physics-440919-t4.Portfolio_Project.Covid_Deaths`
+WHERE 
+  location = 'United States'
+ORDER BY 
+  1,2
 
-select 
-  location, population, Max(total_cases) as Highest_Infection_Count, max((total_cases/population))*100 
-  as Population_Percentage_Infected
-from 
-  'eighth-physics-440919-t4.Portfolio_Project.Covid_Deaths`
-group by 
-  location, population
-order by 
-  Population_Percentage_Infected desc
-
--- Showing Countires with Highest Death Count Per Population
+-- Countries with Highest Infection Rate Per Population --
 
 SELECT 
-    continent,
-    MAX(CAST(total_deaths AS INT)) AS Highest_Death_Count,  -- Maximum deaths per location
-    MAX(CAST(total_deaths AS INT) / population) * 100 AS Population_Percentage_Death  -- Maximum percentage of deaths relative to population
+  location, 
+  population, 
+  Max(total_cases) AS Highest_Infection_Count, 
+  max((total_cases/population))*100 AS Population_Percentage_Infected
 FROM 
-    `eighth-physics-440919-t4.Portfolio_Project.Covid_Deaths`
-where 
-continent is not null
-GROUP BY 
-    continent
-ORDER BY 
-    Highest_Death_Count DESC;  -- Order by Highest_Death_Count instead
+  'eighth-physics-440919-t4.Portfolio_Project.Covid_Deaths`
+GROUP BY
+  location, population
+ORDER BY
+  Population_Percentage_Infected desc
 
--- Continents with the Highest Death Count Per Population 
+-- Countries with Highest Death Count Per Population --
+
+SELECT 
+  continent,
+  MAX(CAST(total_deaths AS INT)) AS Highest_Death_Count,  
+  MAX(CAST(total_deaths AS INT) / population) * 100 AS Population_Percentage_Death  
+FROM 
+  `eighth-physics-440919-t4.Portfolio_Project.Covid_Deaths`
+WHERE 
+  continent is not null
+GROUP BY 
+  continent
+ORDER BY 
+  Highest_Death_Count DESC
+  
+-- Continents with the Highest Death Count Per Population --
 SELECT
   continent,
-  max(cast(total_deaths as int)) as Total_Death_Count
+  max(cast(total_deaths as int)) AS Total_Death_Count
 FROM
   `eighth-physics-440919-t4.Portfolio_Project.Covid_Deaths`
 WHERE
@@ -61,10 +96,10 @@ GROUP BY
 ORDER BY
   Total_Death_Count desc
 
---GLOBAL TOTAL CASES AND DEATHS
+-- Global Total Cases and Deaths --
 SELECT
-  sum(new_cases) as total_cases,
-  sum(cast(new_deaths as int)) as total_deaths,
+  sum(new_cases) as Total_Cases,
+  sum(cast(new_deaths as int)) as Total_Deaths,
   sum(cast(new_deaths as int))/sum(new_cases)* 100 as Death_Percentage
 FROM
     `eighth-physics-440919-t4.Portfolio_Project.Covid_Deaths`
@@ -73,14 +108,15 @@ WHERE
 order by
   1,2
 
---Looking at Total Population vs Total Vaccinations 
+-- Total Population vs Vaccinations --
+-- Shows percentage of population that has recieved at least one COVID vaccine
 SELECT 
   dea.continent,
   dea.location,
   dea.date,
   dea.population,
   vac.new_vaccinations,
-  SUM(CAST(vac.new_vaccinations AS INT)) OVER (PARTITION BY dea.location order by dea.location, dea.date) AS rolling_people_vaccinated
+  SUM(CAST(vac.new_vaccinations AS INT)) OVER (PARTITION BY dea.location order by dea.location, dea.date) AS Rolling_People_Vaccinated
 FROM   
   `eighth-physics-440919-t4.Portfolio_Project.Covid_Deaths` dea
 JOIN 
@@ -91,8 +127,7 @@ WHERE
   dea.continent IS NOT NULL
 ORDER BY 
   2,3
-
---Creating CTE to compare population to people vaccinated 
+-- Using CTE to perform Calculation on Partition By in previous query --
 WITH PopVsVac
 AS
 (
@@ -102,7 +137,7 @@ AS
   dea.date,
   dea.population,
   vac.new_vaccinations,
-  SUM(CAST(vac.new_vaccinations AS INT)) OVER (PARTITION BY dea.location order by dea.location, dea.date) AS rolling_people_vaccinated
+  SUM(CAST(vac.new_vaccinations AS INT)) OVER (PARTITION BY dea.location order by dea.location, dea.date) AS Rolling_People_Vaccinated
 FROM   
   `eighth-physics-440919-t4.Portfolio_Project.Covid_Deaths` dea
 JOIN 
@@ -114,11 +149,40 @@ WHERE
 --ORDER BY 2,3
 )
 
-select *,
+SELECT *,
   (rolling_people_vaccinated/population)*100 as Percentage_Vaccinated 
-from PopVsVac
+FROM
+  PopVsVac
+  
+  -- Using Temp Table to Perform Calculation on Partition By in Previous Query -- 
+  BEGIN
+  CREATE TEMPORARY TABLE Temp_Percent_Population_Vaccinated AS
+  WITH Percent_Population_Vaccinated AS (
+    SELECT 
+      dea.continent, 
+      dea.location, 
+      dea.date, 
+      dea.population, 
+      vac.new_vaccinations,
+      SUM(CAST(vac.new_vaccinations AS INT64)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS Rolling_People_Vaccinated
+    FROM 
+      `eighth-physics-440919-t4.Portfolio_Project.Covid_Deaths` dea
+    JOIN 
+      `eighth-physics-440919-t4.Portfolio_Project.Covid_Vaccinations` vac
+      ON dea.location = vac.location
+      AND dea.date = vac.date
+   
+   WHERE dea.continent IS NOT NULL
+  )
 
--- Creating View to store for later visualization 
+  SELECT 
+    *, 
+    (Rolling_People_Vaccinated / population) * 100 AS Percent_Population_Vaccinated
+  FROM 
+    Percent_Population_Vaccinated;
+END;
+
+-- Creating View to Store for Later Visualization --
 
 CREATE VIEW `eighth-physics-440919-t4.Portfolio_Project.PopVsVac` AS
 SELECT 
@@ -127,7 +191,7 @@ SELECT
   dea.date,
   dea.population,
   vac.new_vaccinations,
-  SUM(CAST(vac.new_vaccinations AS INT64)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS rolling_people_vaccinated
+  SUM(CAST(vac.new_vaccinations AS INT64)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS Rolling_People_Vaccinated
 FROM   
   `eighth-physics-440919-t4.Portfolio_Project.Covid_Deaths` dea
 JOIN 
